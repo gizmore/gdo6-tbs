cdc16072936538c2e2dfe3a9d9d92424f0a3c489
gizmore <gizmore@wechall.net>
2021-01-17 12:48:00 +0100

    Import: Change window.top.location instead of window.location (thx Xaav)

--- Install/ImportTBS.php
+++ Install/ImportTBS.php
@@ -397,6 +397,8 @@ final class ImportTBS
         $fileData = preg_replace('#<body([^>]*)>.*</table></td></tr></table></div>#s', '<body$1>', $fileData);
         # Fix URLs
         $fileData = str_replace('/challenges/', $module->wwwPath('challenges/'), $fileData);
+        # Fix windows.location (JS challs)
+        $fileData = str_replace('window.location.href=', 'window.top.location.href=', $fileData);
         
         # Save
         file_put_contents($outPath, $fileData);
