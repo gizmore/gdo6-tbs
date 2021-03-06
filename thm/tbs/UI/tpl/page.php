076a677b3beed0967a096da5d504863ff6230a19
gizmore <gizmore@wechall.net>
2021-03-06 19:04:21 +0100

    6.10.1-r1009_unit_tests_and_docs

--- thm/tbs/UI/tpl/page.php
+++ thm/tbs/UI/tpl/page.php
@@ -21,7 +21,7 @@ $page->loadSidebars();
 	<title><?=Website::displayTitle()?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="index, follow" />
-	<meta name="generator" content="GDO v<?=Module_Core::instance()->gdo_revision?>">
+	<meta name="generator" content="GDO v<?=Module_Core::$GDO_REVISION?>">
 	<?=Website::displayMeta()?>
 	<?=Website::displayLink()?>
   </head>
