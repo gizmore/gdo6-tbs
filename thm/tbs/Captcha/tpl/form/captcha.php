eb63e3ea20a23fdc385e898fa7001bb2a8b29977
gizmore <gizmore@wechall.net>
2021-05-03 10:49:23 +0200

    6.10.1-r1061_tbs_captcha

--- thm/tbs/Captcha/tpl/form/captcha.php
+++ thm/tbs/Captcha/tpl/form/captcha.php
@@ -1,26 +1,11 @@
 <?php
+use GDO\Core\GDT_Template;
+
 /** @var $field \GDO\Captcha\GDT_Captcha **/
 ?>
 <div class="gdo-container<?= $field->classError(); ?>">
-  <span></span>
-  <label></label>
-  <img
-   class="ib gdo-captcha-img"
-   src="<?= $field->hrefCaptcha(); ?>"
-   onclick="this.src='<?= $field->hrefNewCaptcha(); ?>'+(new Date().getTime())" />
-</div>
-<div class="gdo-container<?= $field->classError(); ?>">
   <?= $field->htmlIcon(); ?>
   <label <?=$field->htmlForID()?>><?= t('captcha'); ?></label>
-  <input
-   <?=$field->htmlID()?>
-   class="ib"
-   autocomplete="off"
-   type="text"
-   pattern="[a-zA-Z]{5}"
-   size="5"
-   required="required"
-   <?=$field->htmlFormName()?>
-   value="<?= $field->displayVar(); ?>" />
+  <?=GDT_Template::php('Captcha', 'form/captcha_inner.php', ['field' => $field])?>
   <?= $field->htmlError(); ?>
 </div>
