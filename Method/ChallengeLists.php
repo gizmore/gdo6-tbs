51c96da6d5fd586f449ea080d3cb2e30040f103c
gizmore <gizmore@wechall.net>
2021-11-28 03:22:00 +0100

    6.11.0-r6205

--- Method/ChallengeLists.php
+++ Method/ChallengeLists.php
@@ -32,7 +32,7 @@ final class ChallengeLists extends Method
         {
             $list = ChallengeList::make();
             $_REQUEST['category'] = $category;
-            $response->addField($list->execute());
+            $response->addField($list->executeWithInit());
         }
         
         return $response;
